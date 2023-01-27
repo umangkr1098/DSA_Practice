e82b22e9bc1bc667d08e0037b9e01f722223028c
umangkr1098 <95139666+umangkr1098@users.noreply.github.com>
2023-01-28 00:05:02 +0530

    added factorial program

---
+++ Factorial.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+   int n;
+   int j=1;
+   cin>>n;
+
+   for(int i=1;i<=n;i++){
+    j=j*i;
+    cout<<j<<" ";
+   }
+   cout<<endl;
+   cout<<j<<" ";   
+
+}
