199954eed18b52dde09f29137f8fc25fd9184217
umangkr1098 <95139666+umangkr1098@users.noreply.github.com>
2023-01-27 18:37:35 +0530

    added data types

---
+++ Data_types.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a = 123;
+    cout<<a<<'\n';
+
+    char b='v'; 
+    char b='we'; 
+    cout<<b<<endl;
+
+    bool bl = true;
+    cout<<bl<<endl;
+
+    float f = 1.8;
+    cout<<f<<endl;
+
+    double d = 1.23;
+    cout<<d<<endl;
+
+    int size= sizeof(a);
+    cout<<"size of a is:"<<size<<endl;
+}
