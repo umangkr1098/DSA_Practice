780de8bcbd84289c78c696369a27c900a2a411d5
umangkr1098 <95139666+umangkr1098@users.noreply.github.com>
2023-02-05 20:02:20 +0530

    added Grade system

--- Grade.cpp
+++ Grade.cpp
@@ -6,10 +6,10 @@ char grade(int marks){
      switch(marks/10){
         case 10 :
         case 9 :return 'A';
-         case 8 :return 'B';
-          case 7 :return 'C';
-           case 6 :return 'D';
-           default : return 'E';
+        case 8 :return 'B';
+        case 7 :return 'C';
+        case 6 :return 'D';
+        default : return 'E';
 
     }
 }
