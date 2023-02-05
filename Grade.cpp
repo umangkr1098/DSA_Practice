56f08b8603f49069c5fdb6f470a807a775b9e299
umangkr1098 <95139666+umangkr1098@users.noreply.github.com>
2023-02-05 19:53:51 +0530

    added grade system

---
+++ Grade.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+char grade(int marks){
+     switch(marks/10){
+        case 10 :
+        case 9 :return 'A';
+         case 8 :return 'B';
+          case 7 :return 'C';
+           case 6 :return 'D';
+           default : return 'E';
+
+    }
+}
+
+int main(){
+    int marks;
+    cin>>marks;
+
+   char ans= grade(marks);
+   cout<<"Your grade is: "<<ans<<endl;
+   
+}
