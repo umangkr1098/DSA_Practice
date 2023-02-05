a6f8464a76a8e9ab25514c27fc80c141908783f9
umangkr1098 <95139666+umangkr1098@users.noreply.github.com>
2023-02-05 23:17:09 +0530

    added counting

---
+++ Counting.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void printCounting(int n){
+    for(int i=1;i<=n;i++){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+}
+
+
+int main(){
+    int n;
+    cin>>n;
+
+    printCounting(n);
+    
+}
